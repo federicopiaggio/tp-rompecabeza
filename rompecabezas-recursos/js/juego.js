0b2cce7537600953655cd901bfa873e779e999b6
federicopiaggio <federicopiaggio26@gmail.com>
2019-09-19 09:22:48 -0300

    primeros cambios

--- rompecabezas-recursos/js/juego.js
+++ rompecabezas-recursos/js/juego.js
@@ -1,5 +1,5 @@
 // Arreglo que contiene las intrucciones del juego 
-var instrucciones = [];
+var instrucciones = ["Armá rompecabezas","Mové las fichas","Vos podés"];
 // Arreglo para ir guardando los movimientos que se vayan realizando
 var movimientos = [];
 
@@ -21,7 +21,10 @@ Cada elemento de este arreglo deberá ser mostrado en la lista con id 'lista-ins
 Para eso deberás usar la función ya implementada mostrarInstruccionEnLista().
 Podés ver su implementación en la ultima parte de este codigo. */
 function mostrarInstrucciones(instrucciones) {
-    //COMPLETAR
+    for(i=0; i< instrucciones.length; i++){
+     
+      mostrarInstruccionEnLista(instrucciones[i],idLista);
+    }
 }
 
 /* COMPLETAR: Crear función que agregue la última dirección al arreglo de movimientos
